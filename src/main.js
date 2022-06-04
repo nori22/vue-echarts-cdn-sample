9df4732010cef05e64e2e9e29eb07a25e65a1aad
nori22 <gmnoritake@gmail.com>
2022-06-04 23:11:58 +0900

    :construction: just add data and method

--- src/main.js
+++ src/main.js
@@ -1,3 +1,52 @@
 new Vue({
-  el: '#app'
+  el: '#app',
+  data: {
+    echartsOption: {},
+    selectedPort: "伊東",
+    portList: [
+      {
+        prefecturesCode: "22",
+        portCode: "2",
+        prefecturesName: "静岡県",
+        portName: "伊東",
+      },
+      {
+        prefecturesCode: "17",
+        portCode: "4",
+        prefecturesName: "石川県",
+        portName: "輪島",
+      },
+      {
+        prefecturesCode: "47",
+        portCode: "5",
+        prefecturesName: "沖縄県",
+        portName: "那覇",
+      }
+    ]
+  },
+  created: async function () {
+    const params = {
+      pc: this.portList.find(port => port.portName == this.selectedPort).prefecturesCode,
+      hc: this.portList.find(port => port.portName == this.selectedPort).portCode,
+      yr: 2022,
+      mn: 5,
+      dy: 29,
+      rg: 'day',
+    }
+
+    const query = new URLSearchParams(params)
+    try {
+      // TODO:original backend化 or 他のサンプルAPIに目星をつける Echartsまでできそうな
+      // tide736がCORSでnullとかlocalhostをはじいている、、ので本Repositoryのファイルシステムからのブラウザではfetchできず
+      const response = await fetch(`https://api.tide736.net/get_tide.php?${query}`)
+      const resData = await response.json()
+      if (resData.message !== '正常終了') throw new Error(resData.message)
+      console.log(resData)
+    } catch (err) {
+      // ignore-eslint
+      console.log('Error at tide736 , error message: ', err.message)
+      if (err instanceof Error) return { isAvailable: false }
+      throw err
+    }
+  }
 })
