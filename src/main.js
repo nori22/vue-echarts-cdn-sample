ff68af0d769b365c536b8297accf21ecf6b0c41a
nori22 <gmnoritake@gmail.com>
2022-06-05 23:35:12 +0900

    switch to sample api
    CORSの問題あって、フルオープンなAPIに変更

--- src/main.js
+++ src/main.js
@@ -2,45 +2,13 @@ new Vue({
   el: '#app',
   data: {
     echartsOption: {},
-    selectedPort: "伊東",
-    portList: [
-      {
-        prefecturesCode: "22",
-        portCode: "2",
-        prefecturesName: "静岡県",
-        portName: "伊東",
-      },
-      {
-        prefecturesCode: "17",
-        portCode: "4",
-        prefecturesName: "石川県",
-        portName: "輪島",
-      },
-      {
-        prefecturesCode: "47",
-        portCode: "5",
-        prefecturesName: "沖縄県",
-        portName: "那覇",
-      }
-    ]
+    bitcoinHistorical: []
   },
   created: async function () {
-    const params = {
-      pc: this.portList.find(port => port.portName == this.selectedPort).prefecturesCode,
-      hc: this.portList.find(port => port.portName == this.selectedPort).portCode,
-      yr: 2022,
-      mn: 5,
-      dy: 29,
-      rg: 'day',
-    }
-
-    const query = new URLSearchParams(params)
     try {
-      // TODO:original backend化 or 他のサンプルAPIに目星をつける Echartsまでできそうな
-      // tide736がCORSでnullとかlocalhostをはじいている、、ので本Repositoryのファイルシステムからのブラウザではfetchできず
-      const response = await fetch(`https://api.tide736.net/get_tide.php?${query}`)
+      const response = await fetch('https://api.sampleapis.com/bitcoin/historical_prices')
       const resData = await response.json()
-      if (resData.message !== '正常終了') throw new Error(resData.message)
+      // if (resData.message !== '正常終了') throw new Error(resData.message)
       console.log(resData)
     } catch (err) {
       // ignore-eslint
