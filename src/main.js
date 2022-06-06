4b3ba1ed17a067b34c36f9f8bd613ceb9f7a1518
nori22 <gmnoritake@gmail.com>
2022-06-07 00:28:09 +0900

    fix: echart dom select timing and obj

--- src/main.js
+++ src/main.js
@@ -1,29 +1,28 @@
-var chartDom = document.getElementById('bitCoinFig');
-var myChart = echarts.init(chartDom);
-
 new Vue({
   el: '#app',
   data: {
     bitcoinHistorical: [],
+    echartsOption: {},
+
   },
-  created: async function () {
+  mounted: async function () {
+    let myChart = echarts.init(document.getElementById("bitFig"))
+
     try {
       const response = await fetch('https://api.sampleapis.com/bitcoin/historical_prices')
       const resData = await response.json()
       // if (resData.message !== '正常終了') throw new Error(resData.message)
-      console.log(resData)
       
-      const echartsOption = this.transBitResToEchartsSimpleOption(resData)
-      console.log("echartsOption")
-      console.log(echartsOption)
+      this.echartsOption = this.transBitResToEchartsSimpleOption(resData)
 
-      echartsOption && myChart.setOption(echartsOption)
     } catch (err) {
       // ignore-eslint
       console.log('Error at tide736 , error message: ', err.message)
       if (err instanceof Error) return { isAvailable: false }
       throw err
     }
+
+    this.echartsOption && myChart.setOption(this.echartsOption)
   },
   methods: {
     transBitResToEchartsSimpleOption: function(resData){
