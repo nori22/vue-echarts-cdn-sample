caa6963229272b0baa8f794177d77b2c1c1715c3
nori22 <gmnoritake@gmail.com>
2022-06-08 23:13:08 +0900

    refactor

--- src/main.js
+++ src/main.js
@@ -11,13 +11,10 @@ new Vue({
     try {
       const response = await fetch('https://api.sampleapis.com/bitcoin/historical_prices')
       const resData = await response.json()
-      // if (resData.message !== '正常終了') throw new Error(resData.message)
-      
+      if (!resData) throw new Error(resData.message)
       this.echartsOption = this.transBitResToEchartsSimpleOption(resData)
 
     } catch (err) {
-      // ignore-eslint
-      console.log('Error at tide736 , error message: ', err.message)
       if (err instanceof Error) return { isAvailable: false }
       throw err
     }
@@ -26,28 +23,21 @@ new Vue({
   },
   methods: {
     transBitResToEchartsSimpleOption: function(resData){
-      let output = {
+      return {
         xAxis: {
           type: 'category',
-          data: []
+          data: resData.map(eachDate => eachDate.Date)
         },
         yAxis: {
           type: 'value'
         },
         series: [
           {
-            data: [],
+            data: resData.map(eachDate => eachDate.Price),
             type: 'line'
           }
         ]
       }
-    
-      output.xAxis.data = resData.map(eachDate => eachDate.Date)
-      output.series[0].data = resData.map(eachDate => eachDate.Price)
-
-      console.log(output)
-    
-      return output
     }
   }
 })
